328c25b5658c9274b50fe9cb1932ed7c17926c5c
Aymansaeed9542 <aym9542@gmail.com>
2025-12-13 01:45:50 +0200

    implement more problems

--- modules/index.js
+++ modules/index.js
@@ -1,5 +1,12 @@
 const path = require('path');
 const fs = require('fs')
+const EventEmitter = require("events")
+const emitter = new EventEmitter
+
+const os = require("os");
+
+
+
 // 1-
 
 // function filePath() {
@@ -89,10 +96,68 @@ const fs = require('fs')
 
 
 // const deleteFile = (pth) => { 
-//     const deletedFile = fs.unlink(pth , (err)=>{ 
+// fs.unlink(pth , (err)=>{ 
 //             if(err) throw err ;
 //             console.log("file deleted successfully"); })
 //         } 
-// console.log(deleteFile("text.txt"));
+// deleteFile("text.txt")
 
 // 11
+// const creatFile = (folderName) => {
+//     fs.mkdirSync(folderName)
+//     console.log("success");
+// }
+// creatFile("ayman")
+
+// 12
+// function logMessage(name){
+//     emitter.on("start" ,(name)=>{
+//         console.log(`Welcom ${name}`);
+//     })
+//     emitter.emit("start" , name )
+// }
+
+// logMessage("ayman")
+
+// 13
+// function login(username){
+//     emitter.on("start" ,(name)=>{
+//         console.log(`Welcom ${name}`);
+//     })
+//     emitter.emit("start" , username )
+// }
+// login("ahmed")
+
+// 14
+// function readFileSync(pth){
+// const file =fs.readFileSync(pth , 'utf-8')
+// console.log(file);
+
+// }
+// readFileSync("./text.txt")
+
+// 15
+// function writeFile(pth , content){
+//     fs.writeFile(pth , content, (err) => {
+//         if(err) throw err
+//         console.log("file written successfully");
+//     })
+// }
+// writeFile("text.txt" , "hi ayman")
+
+// 16
+// const checkExists = (path) => {
+//     return fs.existsSync(path);
+// };
+
+// console.log(checkExists("text.txt"));
+
+// 17
+// const getOSInfo = () => {
+//     return {
+//         Platform: os.platform(),
+//         Arch: os.arch()
+//     };
+// };
+
+// console.log(getOSInfo());
