3ba41ccd48b5eb5c749e9df797bffb68800e6fa1
Aymansaeed9542 <aym9542@gmail.com>
2025-12-23 01:39:31 +0200

    implement read , write functionsand create POST end point for add user

--- express-ERD/index.js
+++ express-ERD/index.js
@@ -1,3 +1,32 @@
 const fs = require('fs')
 const express = require('express')
 const app = express()
+app.use(express.json())
+
+function readFile(){
+    const file = fs.readFileSync("./users.json" ,{
+        encoding : "utf-8"
+    })
+    return JSON.parse(file)
+}
+
+function writeFile(data){
+    const file = fs.writeFileSync("./users.json",JSON.stringify(data),'utf-8')
+}
+
+
+app.post("/addUser" , (req,res) =>{
+    const users = readFile()
+    const newUser = req.body
+    let isExist = users.find(user => newUser.email == user.email)
+    if (isExist) {
+        res.json("Email aleady Exists")
+    }
+    users.push(newUser)
+    res.json(newUser)
+    writeFile(users)
+})
+
+app.listen(3000 , () =>{
+    console.log("server is runnng in port 3000");
+})
